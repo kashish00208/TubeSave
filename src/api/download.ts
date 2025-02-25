4f9b82d2ab4b540aa371c2aa3c8a8f10a87bd640
kashish00208 <kgupta7827@gmail.com>
2025-02-26 01:38:45 +0530

    api done lets see where it goes

--- src/api/download.ts
+++ src/api/download.ts
@@ -1,17 +1,27 @@
-import ytdlp from "yt-dlp-wrap"
-import { NextResponse , NextRequest } from "next/server";
-import { format } from "path";
-export default function handler(req:NextRequest , res:NextResponse){
-    const url = req.body;
-    if(!url){
-        return res.status(400).json({error:"NO URL PROVIDED"})
+import youtubedl from "youtube-dl-exec";
+import type { NextApiResponse , NextApiRequest } from "next";
+import path from 'path'
+export default async function handler(req:NextApiRequest , res:NextApiResponse){
+    if(req.method==='POST'){
+        const url = req.body;
+        if(!url){
+            return res.status(400).json({ error: 'URL is required.' });
+        }
+        try{
+            //path of video to be saved
+            const output = path.join(process.cwd(),'downloads','downloaded_video.%(ext)s')
+            const options = {
+                output,
+                noCheckCertificate:true
+            };
+            const result = await youtubedl(url,options);
+            res.status(200).json({message:"vedeo downloaded susscefully"})
+        }catch(error){
+            console.error("Error in downloading video",error)
+            res.status(500).json({error:"failed to download video"})
+        }
+    }else{
+        res.status(405).json({error:"Method not allowed"})
     }
-    try{
-        
-    }
-    catch(error){
-
-    }
-    
 
 }
