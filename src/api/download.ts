b8cf16095dc11ff7996c11dfdfa833d564c7685d
kashish00208 <kgupta7827@gmail.com>
2025-02-26 00:55:07 +0530

    changes

--- src/api/download.ts
+++ src/api/download.ts
@@ -1,12 +1,13 @@
-import { error } from "console";
+import ytdlp from "yt-dlp-wrap"
 import { NextResponse , NextRequest } from "next/server";
+import { format } from "path";
 export default function handler(req:NextRequest , res:NextResponse){
     const url = req.body;
     if(!url){
         return res.status(400).json({error:"NO URL PROVIDED"})
     }
     try{
-        const video = await 
+        
     }
     catch(error){
 
