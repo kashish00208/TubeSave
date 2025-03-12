a3031e65b0cc68843e31d8712ac1a24c9bc8066a
kashish00208 <kgupta7827@gmail.com>
2025-03-12 12:27:59 +0530

    navbar-working completed

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const NavBar = () => {
   return (
     <div className="w-full p-10 flex justify-between text-white ">
-      <h1 className="text-2xl">TubeSave</h1>
+      <h1 className="text-2xl font-bold font-sans">TubeSave</h1>
       <div className="text-lg ">
         <button className=" border-white border-2 rounded-lg px-2 mr-2 font-bold">Log In</button>
         <button className="bg-white text-gray-900 rounded-lg font-bold px-2 mr-2">Sign Up</button>
