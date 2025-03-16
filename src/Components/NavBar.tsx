7fb9fdfc3fab00fed39ddfb36b3bd9f57199abfa
kashish00208 <kgupta7827@gmail.com>
2025-03-16 13:27:14 +0530

    removed signup

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -6,8 +6,9 @@ const NavBar = () => {
     <div className="w-full p-4 flex justify-between text-white shadow-2xl">
       <h1 className="text-xl font-bold font-sans">TubeSave</h1>
       <div className="text-md ">
-        <button className=" border-white border-2 rounded-lg px-2 mr-2 font-bold">Log In</button>
-        <button className="bg-white text-gray-900 rounded-lg font-bold px-2 mr-2">Sign Up</button>
+        <button className=" border-white border-2 rounded-lg px-2 mr-2 font-bold">
+          Log In
+        </button>
       </div>
     </div>
   );
