dbc054f88866d656cbd19f5fdeb83fc12f28ae3f
kashish00208 <kgupta7827@gmail.com>
2025-03-16 13:06:33 +0530

    tailwind production issue resolved

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,3 @@
-import type { Config } from "tailwindcss";
-
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,11 +6,8 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      colors: {},
     },
   },
   plugins: [],
-} satisfies Config;
+};
