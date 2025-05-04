a584250e12dea5f40820dd82e7771bd4271f3bf3
kashish00208 <kgupta7827@gmail.com>
2025-05-04 13:25:15 +0530

    Minor changes

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const NavBar = () => {
   return (
     <div className="w-full p-4 flex justify-between text-white shadow-2xl">
-      <h1 className="text-xl font-bold font-sans">TubeSave</h1>
+      <h1 className="text-xl font-bold font-sans">YtExtractor</h1>
       <div className="text-md ">
         <button className=" border-white border-2 rounded-lg px-2 mr-2 font-bold">
           Log In
