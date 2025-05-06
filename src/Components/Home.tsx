2963ae50ae8b2067dc155cf6fecfbaf80f5bf600
kashish00208 <kgupta7827@gmail.com>
2025-05-06 16:42:10 +0530

    Home page text changes

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -5,10 +5,10 @@ const MainPage = () => {
   return (
     <>
       <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
-        <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+        <h2 className="bg-clip-text text-center bg-gradient-to-b from-black to-neutral-800 dark:from-neutral-200 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
           Link. Click. Download. <br /> Your YouTube Shortcut.
         </h2>
-        <p className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
+        <p className="max-w-xl mx-auto text-sm md:text-lg text-black dark:text-neutral-900 text-center">
           Instantly download any YouTube video—just paste the link. Fast,
           fuss-free, and perfect for non-premium users.
         </p>
@@ -16,4 +16,5 @@ const MainPage = () => {
     </>
   );
 };
+
 export default MainPage;
