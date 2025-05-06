e6fb9138679bc579897f27a62d165f98484d279b
kashish00208 <kgupta7827@gmail.com>
2025-05-06 16:04:19 +0530

    feat: enhance NavBar with active link state and update layout

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,15 +1,42 @@
 "use client";
-import React from "react";
-
+import React, { useState } from "react";
 
 const NavBar = () => {
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+
+  const navItems = [
+    { title: "Home", link: "#home" },
+    { title: "How it works", link: "#howItworks" },
+    { title: "FAQ", link: "#faq" },
+    { title: "About", link: "#about" },
+  ];
+
   return (
-    <div className="w-full p-4 flex justify-between text-white shadow-2xl">
-      <h1 className="text-xl font-bold font-sans">YtExtractor</h1>
-      <div className="text-md ">
-        <button className=" border-white border-2 rounded-lg px-2 mr-2 font-bold">
-          Log In
-        </button>
+    <div className="flex justify-between items-center m-5 rounded-2xl px-6 py-4 text-sm bg-black bg-opacity-95 text-white">
+      <div className="text-xl font-bold">TubeSave</div>
+
+      <div className="flex gap-6">
+        {navItems.map((item, i) => (
+          <a
+            key={i}
+            href={item.link}
+            onClick={() => setActiveIndex(i)}
+            className={`rounded-3xl py-2 px-3 transition-colors duration-300 ${
+              activeIndex === i
+                ? "bg-blue-600"
+                : "hover:bg-white hover:text-black"
+            }`}
+          >
+            {item.title}
+          </a>
+        ))}
+      </div>
+
+      <div className="flex gap-2">
+        <button className=" ">SignUp</button>
+        <span>|</span>
+        <button className="">LogIn</button>
       </div>
     </div>
   );
