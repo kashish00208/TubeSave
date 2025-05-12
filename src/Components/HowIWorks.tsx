4ac21bb27c7eef643d044684aa7cf369430b7efb
kashish00208 <kgupta7827@gmail.com>
2025-05-12 20:52:15 +0530

    just created how it works section

---
+++ src/Components/HowIWorks.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HowIWorks = () => {
+  return (
+    <div className='w-full '>
+      <div className='card-1 '></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default HowIWorks
