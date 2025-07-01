a15436ced8c2f6cf302c2e0914081a4eb562f431
kashish00208 <kgupta7827@gmail.com>
2025-07-01 18:25:44 +0530

    feat;Audio downloads

--- Backend/main.py
+++ Backend/main.py
@@ -53,3 +53,35 @@ async def download_video(req: DownloadRequest):
             "error": "Download failed",
             "details": str(e)
         }
+
+app.post("api/downloadMp3") 
+async def download_audio(req:DownloadRequest){
+    if not YOUTUBE_REGEX.match(req.url):
+        return {"error":"Invalid youtube URL"}
+
+    filename = f"audio_{int(time.time)}.mp3"
+    output_path = os.path.join(DOWNLOAD_DIR,filename)
+
+    try:
+        subprocess.run([
+            "yt-dlp",
+            "-x",
+            "--audio-format","mp3",
+            "--audio-quality","0",
+            -"o",output_path,
+            req.url
+        ]check=True)
+
+        return {
+
+            "message":"Audio downloaded successfully",
+            "fileUrl":f"downloads/{filename}"
+
+        }
+
+         except subprocess.CalledProcessError as e:
+        return {
+            "error": "Download failed",
+            "details": str(e)
+        }
+}
